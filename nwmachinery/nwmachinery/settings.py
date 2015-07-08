184922155f5291fdcc1280bc289098867dfe97e4
porschia <tinadwilkerson@gmail.com>
2015-07-08 14:59:51 -0500

    Changed static_root

--- nwmachinery/nwmachinery/settings.py
+++ nwmachinery/nwmachinery/settings.py
@@ -83,5 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,
-'../static'))
+STATIC_ROOT = os.path.join(BASE_DIR,
+'../static')
